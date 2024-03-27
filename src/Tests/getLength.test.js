65d51f8435d4598d38151e7c96e1447c6d7167ee
Am1d1v <dimadiman1adima@gmail.com>
2024-03-27 15:52:04 +0300

    Jest Aliases

--- src/Tests/getLength.test.js
+++ src/Tests/getLength.test.js
@@ -2,9 +2,29 @@ import { getLength } from "../getLength";
 
 describe('Get Length of the provided array', () => {
 
-    it('Should return 0 for an empty array', () => {
+    // Multiple Only Tests
+    it.only('Should return 0 for an empty array', () => {
         const res = getLength([]);
         expect(res).toBe(0);
     });
+
+    it.only('Should return 1 for an empty array', () => {
+        const res = getLength([1]);
+        expect(res).toBe(1);
+    });
+
+    it('Should return 2 for an empty array', () => {
+        const res = getLength([1, 2]);
+        expect(res).toBe(2);
+    });
+
+    it('Should return 0 for an empty array', () => {
+        const res = getLength([1, 2, 3]);
+        expect(res).toBe(3);
+    });
+
+    // todo
+    it.todo('Should return 0 for an empty array');
+
 
 });
