8652b44a1cf1cdca8f10028b84867a23deed8d3d
Am1d1v <dimadiman1adima@gmail.com>
2024-03-27 18:26:40 +0300

    Jest Mocks

--- filterArray.js
+++ filterArray.js
@@ -4,8 +4,10 @@ export function filterArray(array, callback){
     const newArray = [];
 
     for(let i = 0; i < array.length; i++){
-        if(callback(array)){
+        if(callback(array[i])){
             newArray.push(array[i]);
         }
     }
+    return newArray;
+
 }
--- src/Tests/filterArray.test.js
+++ src/Tests/filterArray.test.js
@@ -3,6 +3,30 @@ import { basketWithNoQuantity, filteredBasketWithQuantityOnly } from "../../Mock
 
 describe('filterArray()', () => {
 
+    it('Should not invoke callback if an array is empty', () => {
+        const cb = jest.fn();
 
+        filterArray([], cb);
+
+        expect(cb).toHaveBeenCalledTimes(0);
+    });
+
+    it('Should invoke provided function as many times as the length of an array', () => {
+        const cb = jest.fn();
+
+        const arr = [1, 2, 3];
+
+        filterArray(arr, cb);
+
+        expect(cb).toHaveBeenCalledTimes(arr.length);
+    });
+
+    it('Should filter an array using provided predicate', () => {
+        const hasQuantity = (order) => order.quantity > 0;
+
+        const result = filterArray(basketWithNoQuantity, hasQuantity);
+
+        expect(result).toEqual(filteredBasketWithQuantityOnly);
+    });
 
 });
