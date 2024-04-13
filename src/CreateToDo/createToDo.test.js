2f3d3afec810f3b3154b6baa10ebaf4a5056bd8b
Am1d1v <dimadiman1adima@gmail.com>
2024-04-13 15:23:04 +0300

    Jest Modules Mock

--- src/CreateToDo/createToDo.test.js
+++ src/CreateToDo/createToDo.test.js
@@ -1,7 +1,23 @@
 import { createToDo } from "./createToDo";
 
+jest.mock('uuid', () => ({
+    ...jest.requireActual('uuid'),
+    v4: () => 'abcd'
+}))
+
 describe('createToDo', () => {
 
+    it('Should return todo object with provided title, completed and id', () => {
 
+        const title = 'Jest'
+        const result = createToDo(title);
+        const expectedResult = {
+            title,
+            completed: false,
+            id: 'abcd'
+        }
+
+        expect(result).toEqual(expectedResult);
+    }); 
 
 });
