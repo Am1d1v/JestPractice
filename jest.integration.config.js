9a78e3af93b2f9d1d5bfadde4ae2e6331b275001
Am1d1v <dimadiman1adima@gmail.com>
2024-04-15 11:28:28 +0300

    Jest. Multiple Jest Config

--- jest.integration.config.js
+++ jest.integration.config.js
@@ -1,21 +1,13 @@
 /** @type {import('jest').Config} */
 const config = {
     verbose: true,
-    collectCoverage: true,
+    collectCoverage: false,
     collectCoverageFrom: [
       '<rootDir>/src/**/*.{js, jsx, ts, tsx}',
       '!**/node_modules/**',
       '!<rootDir>/src/**/*.mock.*',
     ],
-    coverageThreshold: {
-      global: {
-        branches: 80,
-        functions: 80,
-        lines: 80,
-        statements: 80
-      }
-    },
-    testMatch: ['<rootDir>/src/**/*.test.js']
+    testMatch: ['<rootDir>/src/**/*.spec.js']
   };
   
   module.exports = config;
